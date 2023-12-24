f70571b5566dac0b9cce18d2a56f9c32fc21927e
Christian Alexander <christian.alexanderato@gmail.com>
2023-12-23 19:05:16 -0800

    index route working

--- pokedex.js
+++ pokedex.js
@@ -1,1 +1,31 @@
+// DEPENDENCIES
+const express = require("express");
+const app = express()
+const morgan = require("morgan");
+const Pokemon = require("./models/pokemon");
 
+// MIDDLE WARE
+app.use(morgan("dev"));
+
+// ROUTES
+
+// Index
+app.get("/", (req, res) => {
+ res.send("Who's that pokemon?")
+})
+// New
+
+// Edit
+
+// Create
+
+// Update
+
+// Destroy
+
+// Show
+
+// LISTENER
+app.listen(3000, () => {
+    console.log("Time to catch them all")
+})
