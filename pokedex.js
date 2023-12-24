18bb35d3e1e9a227856ce0e35f3e5c9bb1b75552
Christian Alexander <christian.alexanderato@gmail.com>
2023-12-23 22:03:30 -0800

    show is working

--- pokedex.js
+++ pokedex.js
@@ -1,19 +1,20 @@
 // DEPENDENCIES
 const express = require("express");
-const app = express()
+const app = express();
 const morgan = require("morgan");
 const Pokemon = require("./models/pokemon");
 
 // MIDDLE WARE
 app.use(morgan("dev"));
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // ROUTES
 
 // Index
 app.get("/", (req, res) => {
- res.render("index.ejs")
-})
+  res.render("index.ejs", { data: Pokemon });
+});
+
 // New
 
 // Edit
@@ -25,8 +26,13 @@ app.get("/", (req, res) => {
 // Destroy
 
 // Show
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const pokemon = Pokemon[id];
+  res.render("show.ejs", { pokemon, id });
+});
 
 // LISTENER
 app.listen(3000, () => {
-    console.log("Time to catch them all")
-})
+  console.log("Time to catch them all");
+});
